84db3971e9d3ce37ab97f59183e8d65b2a6710af
eeshashetty <ekshetty2410@gmail.com>
2020-05-01 23:04:43 +0530

    adjusted boilerplate

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const app = express()
 const port = process.env.PORT || 3000
 const timestampRouter = require('./routers/timestamp')
 
-app.use(express.json())
+// app.use(express.json())
+app.use(express.static('public'))
 app.use(timestampRouter)
-
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+  })  
 app.listen(port, () => {
     console.log('Server is up on port '+port)
 })
--- routers/timestamp.js
+++ routers/timestamp.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = new express.Router()
-const date = require('date-and-time')
-
+ 
 router.get('/api/timestamp/:date_string?', (req, res) => {
     const date_string = req.params.date_string
-    const valid_date = new Date()
+    var valid_date = new Date()
     if(date_string){
         valid_date = isNaN(date_string)?new Date(date_string+' GMT'):new Date(parseInt(date_string))
     }
+    console.log(valid_date)
     
     const utc = valid_date.toUTCString()
     const unix = valid_date.getTime()
