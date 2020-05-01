1628ce57435dc0f0a2c688632d35490c9508648c
eeshashetty <ekshetty2410@gmail.com>
2020-05-01 22:51:31 +0530

    added empty parameter option

--- routers/timestamp.js
+++ routers/timestamp.js
@@ -4,8 +4,11 @@ const date = require('date-and-time')
 
 router.get('/api/timestamp/:date_string?', (req, res) => {
     const date_string = req.params.date_string
-    const valid_date = isNaN(date_string)?new Date(date_string+' GMT'):new Date(parseInt(date_string))
-
+    const valid_date = new Date()
+    if(date_string){
+        valid_date = isNaN(date_string)?new Date(date_string+' GMT'):new Date(parseInt(date_string))
+    }
+    
     const utc = valid_date.toUTCString()
     const unix = valid_date.getTime()
 
