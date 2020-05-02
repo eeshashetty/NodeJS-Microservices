83af729a172b1571fa7b1191b9a372cff0da7c87
eeshashetty <ekshetty2410@gmail.com>
2020-05-02 15:16:44 +0530

    updated ip for header parser

--- routers/headerparser.js
+++ routers/headerparser.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const router = new express.Router()
-const ip = require('public-ip')
 
 router.get('/api/whoami', async(req, res) => {
     const headerdata = req.headers
     res.send({
-        ipaddress: await ip.v4(),
+        ipaddress: req.ip,
         language: headerdata['accept-language'],
         software: headerdata['user-agent']
     })
